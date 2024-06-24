0bb48581da146a3027b2d322b92cc142218c0bf8
♯ЅᗩFᏒOT꙯ <168010708+safrotbob2@users.noreply.github.com>
2024-06-24 10:42:39 +0300

    Add files via upload

---
+++ plugins/كود1.js
@@ -0,0 +1,54 @@
+let handler = async (m, { conn, args, usedPrefix, command }) => {      
+    conn.relayMessage(m.chat, {
+      viewOnceMessage: {
+        message: {
+          interactiveMessage: {
+            header: {
+              title: '*♯ЅᗩFᏒOT꙯ 🧚‍♂️📌*'
+            },
+            body: {
+              text: 'طول مانت قلبك ابيض عشتك هتبقا سوده🧸🖤'
+            },
+            nativeFlowMessage: {
+              buttons: [
+                {
+                  name: 'single_select',
+                  buttonParamsJson: JSON.stringify({
+                    title: '💜🧸',
+                    sections: [
+                      {
+                        title: 'List',
+                        highlight_label: 'yas',
+                        rows: [
+                          {
+                            header: 'قسم الاوامر🤺🕊',
+                            title: '',
+                            description: '',
+                            id: '.اوامر'
+                          },
+                          {
+                            header: 'رقم المطور🤺🕊',
+                            title: '',
+                            description: '',
+                            id: '.المطور'
+                          }
+                        ]
+                      }
+                    ]
+                  }),
+                  messageParamsJson: ''
+                }
+              ]
+            }
+          }
+        }
+      }
+    }, {})
+
+}
+
+handler.help = ['info']
+handler.tags = ['main']
+handler.command = ['سفروت']
+
+export default handler
