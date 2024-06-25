ff44d3f18b5d4f8f4f364e9df9d05e679193376e
♯ЅᗩFᏒOT꙯ <168010708+safrotbob2@users.noreply.github.com>
2024-06-25 14:03:08 +0300

    Update تصويت.js

--- plugins/تصويت.js
+++ plugins/تصويت.js
@@ -2,8 +2,8 @@ const handler = async (m, {conn, text, args, usedPrefix, command}) => {
   let name = await conn.getName(m.sender);
   if (name == 'undefined') name = 'Indefinido';
   const b = text.split('|');
-  if (!b[1]) throw `*⚠️ الامر بيشتغل كدا ${usedPrefix + command} الافضل؟|ايتاتشي|مادرا*`;
-  if (b[12]) throw `*⚠️ الامر بيشتغل كدا  ${usedPrefix + command} الافضل؟|ايتاتشي|مادرا*`;
+  if (!b[1]) throw `*⚠️ الامر بيشتغل كدا ${usedPrefix + command} مثال؟|سفروت|بوت*`;
+  if (b[12]) throw `*⚠️ الامر بيشتغل كدا  ${usedPrefix + command} مثال؟|سفروت|بوت*`;
   const caption = `*مقدم التصويت:*\n${name}\n*السؤال ↧↧:*\n${text.split('|')[0]}`.trim();
   const options = text.split("|").slice(1).map(option => ({ optionName: option.trim() }));  
   const sendPollMessage = {
