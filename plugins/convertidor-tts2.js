b25496c596b4847271440242951c47cf77efec9c
♯ЅᗩFᏒOT꙯ <168010708+safrotbob2@users.noreply.github.com>
2024-06-27 22:48:27 +0300

    Update convertidor-tts2.js

--- plugins/convertidor-tts2.js
+++ plugins/convertidor-tts2.js
@@ -1,116 +1,34 @@
-/*//////////////////////////////////////////////
+import fetch from 'node-fetch'; 
 
-        [ ❗ ] CREDITOS - NO MODIFICAR [ ❗ ]
+let handler = async function (m, { text }) {
 
-           Codigo hecho por @BrunoSobrino
-       Github: https://github.com/BrunoSobrino
-       
-       Nota: Solo hay disponibles efectos en
-       ingles, por lo que el texto en otros
-       idiomas puede sonar raro.
-       
-//////////////////////////////////////////////*/
-
-import axios from 'axios';
-import fetch from 'node-fetch';
-
-const handler = async (m, { conn, usedPrefix, command, text, args }) => {
   const datas = global
   const idioma = datas.db.data.users[m.sender].language
   const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
-  const tradutor = _translate.plugins.convertidor_tts2
+  const tradutor = _translate.BK9.BK9
 
-
-  const [efecto, ...textoArray] = text.split(" ");
-  const texto = textoArray.join("");
-
-  if (!efecto) {
-    let voiceList = await getVoiceList();
-    let responseText = `*${tradutor.texto1}*\n`;
-
-    for (let i = 0, count = 0; count < 100 && i < voiceList.resultado.length; i++) {
-      const entry = voiceList.resultado[i];
-      if (entry.ID.length <= 20) {
-        responseText += `*◉ ${usedPrefix + command} ${entry.ID} ${tradutor.texto2}*\n`;
-        count++;
-      }
+  try {
+    if (!text) {
+      m.reply(`${tradutor.bk9LText_T}`);
+      return;
     }
+    const [link, alias] = text.split("+").map(part => part.trim());
+    let apiUrl = `https://bk9.site/api/create?url=${encodeURIComponent(link)}`;
+    if (alias) apiUrl += `&alias=${encodeURIComponent(alias)}`;
+    const response = await fetch(apiUrl);
+    const data = await response.json();
 
-    return conn.sendMessage(m.chat, { text: responseText.trim() }, { quoted: m });
-  }
-
-  let efectoValido = false;
-  let voiceList = await getVoiceList();
-  for (const entry of voiceList.resultado) {
-    if (entry.ID === efecto) {
-      efectoValido = true;
-      break;
+    if (data.BK99) {
+      return m.reply(`${tradutor.bk9LTaked_T}`);
     }
+    const shortURL = data.BK9;
+    return m.reply(`${shortURL}`);
+  } catch (error) {
+    console.error(error);
+    return m.reply(`${tradutor.bk9err}`);
   }
-
-  if (!efectoValido) return conn.sendMessage(m.chat, { text: `*${tradutor.texto3[0]} ${usedPrefix + command} ${tradutor.texto3[1]}*` }, { quoted: m });
-
-  if (!texto) return conn.sendMessage(m.chat, {text: `*${tradutor.texto4[0]}*\n*◉ ${usedPrefix + command} ${efecto} ${tradutor.texto4[1]}*`}, {quoted: m});
-
-  let masivo = await makeTTSRequest(texto, efecto);
-  conn.sendMessage(m.chat, {audio: {url: masivo.resultado}, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
 };
 
-handler.command = /^(g?tts2)$/i;
+handler.command = ['bk9short'];
+handler.tags = ['tools'];
 export default handler;
-
-const secretKey = 'fe2ee40099494579af0ecf871b5af266';
-const userId = 'SrgwcKcLzSY63IdsAxd1PzscFjL2';
-
-async function getVoiceList() {
-  const url = 'https://play.ht/api/v2/voices';
-  const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      AUTHORIZATION: `Bearer ${secretKey}`,
-      'X-USER-ID': userId
-    }
-  };
-  try {
-    const response = await fetch(url, options);
-    const responseData = await response.json(); 
-    const uniqueData = responseData.reduce((acc, current) => {
-      if (!acc.some(item => item.id === current.id)) {
-        acc.push(current);
-      }
-      return acc;
-    }, []);
-    const simplifiedList = uniqueData.map(entry => ({
-      ID: entry.id,
-      name: entry.name,
-      lenguaje: entry.language  
-    }));
-    return { resultado: simplifiedList ? simplifiedList : `${tradutor.texto5}` };
-  } catch (error) {
-    console.error('Error:', error);
-    return { resultado: `${tradutor.texto6}` };
-    throw error;
-  }
-}
-
-async function makeTTSRequest(texto, efecto) {
-  const requestData = {text: texto, voice: efecto};
-  const headers = {
-    'Authorization': `Bearer ${secretKey}`,
-    'X-User-Id': userId,
-    'accept': 'text/event-stream',
-    'content-type': 'application/json'
-  };
-  try {
-    const response = await axios.post('https://play.ht/api/v2/tts', requestData, { headers });
-    const events = response.data.split('\r\n\r\n');
-    const eventData = events.find(event => event.includes('"stage":"complete"'));
-    const urlMatch = eventData.match(/"url":"([^"]+)"/);
-    const url = urlMatch ? urlMatch[1] : null;
-    return { resultado: url ? url : `${tradutor.texto7}` };
-  } catch (error) {
-    console.error('Error:', error);
-    return { resultado: '[❗] Error, no se obtuvo respuesta de la API.' };
-  }
-}
