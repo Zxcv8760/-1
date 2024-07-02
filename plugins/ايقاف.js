b98ea0d407673adddd936a30ced9b20c6b32ce85
♯ЅᗩFᏒOT꙯ <168010708+safrotbob2@users.noreply.github.com>
2024-07-02 19:53:00 +0300

    Create ايقاف.js

---
+++ plugins/ايقاف.js
@@ -0,0 +1,16 @@
+const handler = async (m) => {
+    global.db.data.chats[m.chat].isBanned = true;
+    global.db.data.chats[m.chat].welcome = false;
+    global.db.data.chats[m.chat].detect = false;
+    global.db.data.chats[m.chat].modohorny = false;
+    global.db.data.chats[m.chat].server = false;
+    global.db.data.chats[m.chat].session = false;
+
+    global.db.data.chats[m.chat].audios = false;
+    m.reply('*يبدو انك استعملت البوت بدون اذن*\n> *لقد تم ايقاف استعمال البوت في هذه الدردشه*\n*سفروت🕊*');
+    };
+    handler.help = ['banchat'];
+    handler.tags = ['owner'];
+    handler.command = /ايقاف$/i;
+    handler.rowner = true;
+    export default handler;
