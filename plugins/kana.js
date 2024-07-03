f773ccb1debd2d4ccfe409705bd506e188b4f5a2
♯ЅᗩFᏒOT꙯ <168010708+safrotbob2@users.noreply.github.com>
2024-07-03 20:27:28 +0300

    Update kana.js

--- plugins/kana.js
+++ plugins/kana.js
@@ -1,11 +1,11 @@
 import fs from 'fs';
 const handler = (m) => m;
 handler.all = async function(m) {
-  const vn = './media/خطتولي.mp3';
+  const vn = './media/امك.mp3';
   const chat = global.db.data.chats[m.chat];
-  if (/^فين سفروت$/i.test(m.text) && !chat.isBanned) {
+  if (/^كسم البوت|كسمك يا سفروت|كسم سفروت|بوت ابن متناكه|بوت عرص|بوت خول|بوت ابن زانيه$/i.test(m.text) && !chat.isBanned) {
     conn.sendPresenceUpdate('recording', m.chat);
-    await m.reply(`*انا هنا اهو يا قلبي🧸💜*`);
+    await m.reply(`*كسمك يابن المره المتناكه متشتمش البوت🫵🏻*`);
     m.conn.sendMessage(m.chat, {audio: {url: vn}, fileName: 'error.mp3', mimetype: 'audio/mpeg', ptt: true}, {quoted: m});
   }
   return !0;
